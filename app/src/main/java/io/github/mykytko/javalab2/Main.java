969cfeb780bdd39a5edfe8fbaa1c5e8981e79cda
mykytko <nd235471@gmail.com>
2022-11-03 13:22:32 +0200

    Implement Main

--- app/src/main/java/io/github/mykytko/javalab2/Main.java
+++ app/src/main/java/io/github/mykytko/javalab2/Main.java
@@ -1,9 +1,5 @@
 package io.github.mykytko.javalab2;
 
-import javafx.util.Pair;
-import java.util.ArrayList;
-import java.util.Random;
-
 import io.github.mykytko.javalab2.factories.AbstractFactory;
 import io.github.mykytko.javalab2.factories.BaseFactory;
 import io.github.mykytko.javalab2.factories.UnsupportedColorException;
@@ -13,13 +9,48 @@ import io.github.mykytko.javalab2.primitives.circle.Circle;
 import io.github.mykytko.javalab2.primitives.triangle.InvalidPointsAmountException;
 import io.github.mykytko.javalab2.primitives.triangle.Triangle;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-    
+    private static class Pair<T1, T2> {
+        public T1 first;
+        public T2 second;
+
+        public Pair(T1 first, T2 second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        @Override
+        public String toString() {
+            return "Pair(" + first.toString() + ", " + second.toString() + ")";
+        }
     }
 
-    private static Pair <Circle, Triangle> TestFactory(BaseFactory factory, Random rand) {
-    	var points = new ArrayList<Point>();
+    public static void main(String[] args) {
+        BaseFactory blackFactory;
+        BaseFactory whiteFactory;
+
+        try {
+            blackFactory = AbstractFactory.getFactory(Color.BLACK);
+            whiteFactory = AbstractFactory.getFactory(Color.WHITE);
+        } catch (UnsupportedColorException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        Random rand = new Random();
+
+        var test1 = TestFactory(blackFactory, rand);
+        var test2 = TestFactory(whiteFactory, rand);
+
+        System.out.println(test1);
+        System.out.println(test2);
+    }
+
+    private static Pair<Circle, Triangle> TestFactory(BaseFactory factory, Random rand) {
+        var points = new ArrayList<Point>();
 
         for(var i = 0; i < 4; i++) {
             var x = (float) ((float) (rand.nextInt(2000) - 1000) / 10.0);
--- app/src/main/java/io/github/mykytko/javalab2/factories/UnsupportedColorException.java
+++ app/src/main/java/io/github/mykytko/javalab2/factories/UnsupportedColorException.java
@@ -1,7 +1,4 @@
 package io.github.mykytko.javalab2.factories;
 
 public class UnsupportedColorException extends Exception {
-  public InvalidFactoryColorException() {
-    super("UnsupportedColorException");
-  }
 }
